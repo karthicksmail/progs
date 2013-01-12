762277f5cbc240c6529201825e7d5d2a21513d0b
Karthick Sundararajan <karthicksmail@gmail.com>
2013-01-12 15:11:11 +0800

    Create specified Checker Board using Asterisk

---
+++ java/jhtp/chap4/AsteriskCheckerBoard.java
@@ -0,0 +1,14 @@
+public class AsteriskCheckerBoard {
+	public static void main(String[] args) {
+		for (int i = 0; i < 8; i++) {
+			if (i % 2 == 1) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 8; j++) {
+				System.out.print( "* " );
+			}
+			System.out.println();
+		}
+	}
+}
+
