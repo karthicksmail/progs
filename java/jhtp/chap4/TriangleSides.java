819144b88edcc28c549c12430ee53881153a5dd4
Karthick Sundararajan <karthicksmail@gmail.com>
2013-01-12 16:49:20 +0800

    See if the lengths make a triangle

---
+++ java/jhtp/chap4/TriangleSides.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TriangleSides {
+	public static void main(String[] args) {
+		int s1, s2, s3;
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the length of the three sides: ");
+		s1 = input.nextInt();
+		s2 = input.nextInt();
+		s3 = input.nextInt();
+		
+		if ( ((s1 + s2) > s3) && ((s2 + s3) > s1) && ((s3 + s1) > s2) ) {
+			System.out.println("These three lengths make a triangle");
+		} else {
+			System.out.println("These three lengths do not make a triangle");
+		}
+	}
+}
+
