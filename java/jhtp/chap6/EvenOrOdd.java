7b5c63db3ad9050b438a7569f351ec719c57462e
Karthick Sundararajan <karthicksmail@gmail.com>
2013-05-01 15:32:42 +0800

    Checks if a number is even or odd and returns the same

---
+++ java/jhtp/chap6/EvenOrOdd.java
@@ -0,0 +1,25 @@
+/*
+(Even or Odd) Write a method isEven that uses the remainder operator (%) to determine whether an integer is even. The method should take an integer argument and return true if the integer is even and false otherwise. Incorporate this method into an application that inputs a sequence of integers (one at a time) and determines whether each is even or odd.
+*/
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the number: ");
+		int num = input.nextInt();
+
+		System.out.println("The number " + num + " is " + ((isEven(num) == true)?"even":"odd") + ".");
+	}
+
+	public static boolean isEven(int num) {
+		if(num % 2 == 0) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
