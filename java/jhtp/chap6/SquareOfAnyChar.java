65e83efe38e6ddc7fa8d09cddfaad76893328873
Karthick Sundararajan <karthicksmail@gmail.com>
2013-05-01 16:46:18 +0800

    Print a Square of any specific character

---
+++ java/jhtp/chap6/SquareOfAnyChar.java
@@ -0,0 +1,32 @@
+/*
+(Displaying a Square of Any Character) Modify the method created in Exercise 6.18 to receive a second parameter of type char called fillCharacter. Form the square using the char provided as an argument. Thus, if side is 5 and fillCharacter is #, the method should display
+*/
+import java.util.Scanner;
+
+public class SquareOfAnyChar {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the number of asterisk in each side of the square: ");
+		int size = input.nextInt();
+
+		System.out.print("Enter the character to be printed: ");
+		char ch = input.next().toLowerCase().charAt(0);
+
+		squareOfAnyChars(size, ch);
+	}
+
+	public static void squareOfAnyChars(int size, char ch) {
+		for (int i = 0 ; i < size; i++) {
+			lineOfAnyChars(size, ch);
+		}
+	}
+
+	public static void lineOfAnyChars(int size, char ch) {
+		for (int j = 0; j < size; j++) {
+			System.out.print(ch);
+		}
+		System.out.println("");
+	}
+}
+
